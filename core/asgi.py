1094c873e319a5c6903b307de02dcab24216fda0
George <42513185+RympeR@users.noreply.github.com>
2021-10-22 00:31:39 +0300

    update asgi

--- core/asgi.py
+++ core/asgi.py
@@ -9,20 +9,21 @@ from django.urls import re_path
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 django_asgi_app = get_asgi_application()
 
-from apps.chat import consumers
+from apps.chat.consumers import ChatConsumer, ReadedConsumer, \
+    LastMessageConsumer, ChatRoomsConsumer
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
 
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            re_path(r"ws/chat/(?P<room_name>\w+)/", consumers.ChatConsumer.as_asgi()),
+            re_path(r"ws/chat/(?P<room_name>\w+)/", ChatConsumer.as_asgi()),
             re_path(r'ws/chat-readed/(?P<room_name>\w+)/(?P<user_id>\w+)/$',
-                    consumers.ReadedConsumer.as_asgi()),
+                    ReadedConsumer.as_asgi()),
             re_path(r'ws/last-message/(?P<room_name>\w+)/$',
-                    consumers.LastMessageConsumer.as_asgi()),
+                    LastMessageConsumer.as_asgi()),
             re_path(r'ws/chat-rooms/(?P<user_id>\w+)/$',
-                    consumers.ChatRoomsConsumer.as_asgi()),
+                    ChatRoomsConsumer.as_asgi()),
         ])
     ),
 })
