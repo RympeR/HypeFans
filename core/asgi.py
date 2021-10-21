146cc78da01a5088e0ea2ce2cf90fc3e4084be38
George <42513185+RympeR@users.noreply.github.com>
2021-10-22 00:37:03 +0300

    update asgi

--- core/asgi.py
+++ core/asgi.py
@@ -1,7 +1,5 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.conf.urls import url
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
@@ -9,7 +7,11 @@ from django.urls import re_path
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 django_asgi_app = get_asgi_application()
 
-from apps.chat.consumers import ChatConsumer
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from apps.chat.consumers import ChatConsumer, ReadedConsumer, \
+    LastMessageConsumer, ChatRoomsConsumer
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
@@ -17,12 +19,12 @@ application = ProtocolTypeRouter({
     "websocket": AuthMiddlewareStack(
         URLRouter([
             re_path(r"ws/chat/(?P<room_name>\w+)/", ChatConsumer.as_asgi()),
-            # re_path(r'ws/chat-readed/(?P<room_name>\w+)/(?P<user_id>\w+)/$',
-            #         ReadedConsumer.as_asgi()),
-            # re_path(r'ws/last-message/(?P<room_name>\w+)/$',
-            #         LastMessageConsumer.as_asgi()),
-            # re_path(r'ws/chat-rooms/(?P<user_id>\w+)/$',
-            #         ChatRoomsConsumer.as_asgi()),
+            re_path(r'ws/chat-readed/(?P<room_name>\w+)/(?P<user_id>\w+)/$',
+                    ReadedConsumer.as_asgi()),
+            re_path(r'ws/last-message/(?P<room_name>\w+)/$',
+                    LastMessageConsumer.as_asgi()),
+            re_path(r'ws/chat-rooms/(?P<user_id>\w+)/$',
+                    ChatRoomsConsumer.as_asgi()),
         ])
     ),
 })
