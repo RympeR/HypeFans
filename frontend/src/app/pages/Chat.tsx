511ef9e8334609e616ea9ce48d3dd349ffabdb08
alexeybelyaevv <alex05lev08@gmail.com>
2021-10-28 20:20:43 +0200

    routesInChat/webSocketGetChats

--- frontend/src/app/pages/Chat.tsx
+++ frontend/src/app/pages/Chat.tsx
@@ -1,30 +1,70 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import moment from 'moment';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link, Route, useHistory } from 'react-router-dom';
+import { RootState } from '~/redux/redux';
 import { ReactComponent as BackIcon } from '../../assets/images/arrow-left.svg';
 import { ReactComponent as PlusIcon } from '../../assets/images/plus.svg';
-import { ReactComponent as Vektor } from '../../assets/images/send.svg';
 import { ReactComponent as UsersIcon } from '../../assets/images/users.svg';
+import { getMainUrlPoint } from '../utils/utilities';
+import { DialogMain } from './DialogMain';
 import { NoDialog } from './NoDialog';
 
 const Chat = () => {
+  const userId = useSelector((state: RootState) => state.auth.pk);
   const history = useHistory();
   const BackButton = () => <BackIcon onClick={history.goBack} />;
   const Plus = () => <PlusIcon />;
   const UserIcon = () => <UsersIcon />;
-  const VektorIcon = () => <Vektor />;
+  const [rooms, setRooms] = useState([]);
+  const [isSended, setSended] = useState(false);
+  const isLoading = useSelector((state: RootState) => state.blog.isLoading);
 
-  const SidebarItem = () => {
+  if (isLoading) {
+    return <div>fdfd</div>;
+  }
+
+  const ws = new WebSocket(`wss://hype-fans.com/ws/chat-rooms/${userId}/`);
+
+  ws.onopen = () => {
+    if (isSended) {
+      return;
+    } else {
+      setSended(true);
+      ws.send(
+        JSON.stringify({
+          room: 0
+        })
+      );
+    }
+  };
+
+  ws.onmessage = (e) => {
+    return setRooms(JSON.parse(e.data).room);
+  };
+
+  const SidebarItem = (item: any) => {
+    const history = useHistory();
+    const lastUrl = getMainUrlPoint(history.location.pathname);
     return (
-      <div style={{ display: 'flex', borderBottom: '1px solid rgba(0, 0, 0, 0.2)' }}>
-        <div className="chat__sidebarItem">
-          <img src="https://avatars1.githubusercontent.com/u/2048511?v=4" alt="fdsfsdfsd"></img>
-          <div>
-            <h2>Aleksej Bel`ajev</h2>
-            <p>Привет!!!</p>
+      <Link to={`/chat/${item?.item?.room?.id}`}>
+        <div
+          style={
+            history.location.pathname === '/chat'
+              ? { display: 'flex', borderBottom: '1px solid rgba(0, 0, 0, 0.2)' }
+              : { display: 'flex', borderBottom: '1px solid rgba(0, 0, 0, 0.2)' }
+          }
+        >
+          <div className="chat__sidebarItem">
+            <img src={item?.item?.room?.user?.avatar} alt="fdsfsdfsd"></img>
+            <div>
+              <h2>{item?.item?.room?.user?.first_name}</h2>
+              <p>{item?.item?.room?.message?.text}</p>
+            </div>
           </div>
+          <p className="chat__p">{moment(item?.item?.room?.message?.time).format('L')}</p>
         </div>
-        <p className="chat__p">15 минут назад</p>
-      </div>
+      </Link>
     );
   };
 
@@ -46,24 +86,16 @@ const Chat = () => {
       </div>
       <div className="chat__main">
         <div className="chat__sidebar">
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
-          <SidebarItem />
+          {rooms.map((item, key) => {
+            return (
+              <div key={key}>
+                <SidebarItem item={item} />
+              </div>
+            );
+          })}
         </div>
-        {/* <DialogMain /> */}
-        <NoDialog />
+        <Route path="/chat/:id" component={DialogMain} exact />
+        <Route path="/chat" component={NoDialog} exact />
       </div>
     </div>
   );
--- frontend/src/app/pages/NoDialog.tsx
+++ frontend/src/app/pages/NoDialog.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 
 export const NoDialog = () => {
+  const [currentTab, setCurrentTab] = useState(0);
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', width: '62.5%', paddingTop: '10%' }}>
-      <div style={{ textAlign: 'center' }}>Выберите диалог или создайте новый</div>
-      <button className="notifications__settingBtn" style={{ marginTop: '20px' }}>
-        Новое сообщение
-      </button>
-    </div>
+    <>
+      <div style={{ display: 'flex', flexDirection: 'column', width: '62.5%', paddingTop: '10%' }}>
+        <div style={{ textAlign: 'center' }}>Выберите диалог или создайте новый</div>
+        <button className="notifications__settingBtn" style={{ marginTop: '20px' }} onClick={() => setCurrentTab(1)}>
+          Новое сообщение
+        </button>
+      </div>
+      <Modal show={currentTab !== 0} onHide={() => setCurrentTab(0)} centered>
+        <Modal.Body className="notifications__modal">
+          {currentTab === 1 ? (
+            <>
+              <h2 style={{ marginBottom: '0px' }}>Создание беседы</h2>
+              <div style={{ display: 'flex', justifyContent: 'center', marginTop: '15px' }}>
+                <h3 onClick={() => setCurrentTab(currentTab - 1)} style={{ color: '#FB5734' }}>
+                  Отмена
+                </h3>
+                <div style={{ width: '20px' }}></div>
+                <h3 onClick={() => setCurrentTab(currentTab + 1)}>Далее</h3>
+              </div>
+            </>
+          ) : null}
+          {currentTab === 2 ? (
+            <>
+              <h2 style={{ marginBottom: '0px' }}>беседы</h2>
+              <div style={{ display: 'flex', justifyContent: 'center', marginTop: '15px' }}>
+                <h3 onClick={() => setCurrentTab(currentTab - 1)} style={{ color: '#FB5734' }}>
+                  Отмена
+                </h3>
+                <div style={{ width: '20px' }}></div>
+                <h3 onClick={() => setCurrentTab(currentTab + 1)}>Далее</h3>
+              </div>
+            </>
+          ) : null}
+          {currentTab === 3 ? (
+            <>
+              <h2 style={{ marginBottom: '0px' }}>Созда</h2>
+              <div style={{ display: 'flex', justifyContent: 'center', marginTop: '15px' }}>
+                <h3 onClick={() => setCurrentTab(currentTab - 1)} style={{ color: '#FB5734' }}>
+                  Отмена
+                </h3>
+                <div style={{ width: '20px' }}></div>
+                <h3 onClick={() => setCurrentTab(0)}>Далее</h3>
+              </div>
+            </>
+          ) : null}
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
