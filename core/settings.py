894912142439ae9aaa82ed468fb467441bbf7957
George <georg.rashkov@gmail.com>
2022-01-08 00:02:10 +0200

    cors settings update

--- core/settings.py
+++ core/settings.py
@@ -203,10 +203,22 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
 }
-
+SESSION_COOKIE_SAMESITE = None
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "x-authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 CORS_ALLOW_METHODS = (
     'GET',
     'POST',
