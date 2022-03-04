a0bcd26feee3ac10cd370b81cbccb307b7b679e1
George <42513185+RympeR@users.noreply.github.com>
2022-03-04 23:57:24 +0200

    minor scrollbar style + deplot script update

--- deploy.bat
+++ deploy.bat
@@ -3,9 +3,13 @@ yarn build
 rmdir -r ../build
 mv -Force build ../build
 cd ..
+git add .
+git commit -am "rebuild"
+git push origin N/frontend
+git checkout main
+git pull origin N/frontend
+venv/Scripts/activate
 python .\manage.py collectstatic --no-input
 git add .
-git commit -am "minor audio rebuild"
-git checkout main
-git merge
+git commit -am "main branch rebuild"
 git push origin
