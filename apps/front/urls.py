09ae796c4209a171d5818a1634433557c71683f9
George <42513185+RympeR@users.noreply.github.com>
2022-03-04 22:04:50 +0200

    update files

--- apps/front/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, re_path
-from .views import (
-    IndexView,
-)
-urlpatterns = [
-    re_path('^.*$',IndexView.as_view()),
-]
--- core/utils/customFields.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-
-class TimestampField(serializers.Field):
-    def to_representation(self, value):
-        return value.timestamp()
-
-    def to_internal_value(self, value):
-        return value
