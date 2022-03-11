feceb85f136c63b64df48ca72e5afda114b0bc33
alexeybelyaevv <alex05lev08@gmail.com>
2022-03-11 19:52:25 +0200

    pagination

--- frontend/src/app/pages/Notifications.tsx
+++ frontend/src/app/pages/Notifications.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "react-phone-input-2/lib/style.css";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Route, useHistory } from "react-router-dom";
-import { getNotifications } from "../../redux/notificationsReducer";
+import { getNotifications, updateNotifications } from "../../redux/notificationsReducer";
 import { RootState } from "../../redux/redux";
 import { ReactComponent as BackIcon } from "../../assets/images/arrow-left.svg";
 import { ReactComponent as BellIcon } from "../../assets/images/bell.svg";
@@ -15,6 +15,8 @@ import { DefaultSidebar } from "../components/notificationsComponents/DefaultSid
 import { SidebarText } from "../components/notificationsComponents/SidebarText";
 import { Preloader } from "../utils/Preloader";
 import { Notification } from "./notifications/Notification";
+import axios from "axios";
+import { blogAPI } from "../../api/blogAPI";
 
 const Notifications: React.FC = () => {
   const dispatch = useDispatch();
@@ -25,12 +27,16 @@ const Notifications: React.FC = () => {
   const isLoading = useSelector(
     (state: RootState) => state.notifications.isLoading
   );
+
+
   useEffect(() => {
     dispatch(getNotifications());
   }, [dispatch]);
+
   if (isLoading) {
     return <Preloader />;
   }
+
 
   const articles = [
     {
@@ -108,12 +114,35 @@ const Notifications: React.FC = () => {
       }
     }, []);
     const Main = ({ notifications }: { notifications: Array<any> }) => {
+
+      const [page, setPage] = useState<number>(2)
+
+      const [data, setData] = useState([...notifications])
+
+      console.log([data]);
+
+
+
+      const onScrollList = async (event: any) => {
+        const scrollBottom = event.target.scrollTop +
+          event.target.offsetHeight === event.target.scrollHeight;
+
+        if (scrollBottom) {
+          const response = await blogAPI.getNotifications({ limit: 5, offset: page })
+          setData([...data, ...response.data])
+          setPage(page + 1)
+        }
+      }
+
+
       return (
-        <div className="notifications__main">
-          {notifications.length > 0 ? (
-            notifications.map((item, i) => {
-              return <Notification key={`notification ${i}`} item={item} />;
-            })
+        <div className="notifications__main" onScroll={event => onScrollList(event)}>
+          {data.length > 0 ? (
+            <>
+              {data.map((item, i) => {
+                return <Notification key={`notification ${i}`} item={item} />;
+              })}
+            </>
           ) : (
             <div
               style={{
