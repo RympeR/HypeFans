a546d7fb1ae983ed4cafa65ce72cb36bf34a8368
George <42513185+RympeR@users.noreply.github.com>
2022-03-27 23:46:17 +0200

    main page update

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -582,7 +582,8 @@ class MainUserPageUpdated(APIView):
 
         qs = User.objects.all().order_by('-fans_amount').values_list('id', flat=True)
         reccomendations = UserShortRetrieveSeriliazer(
-            instance=self.get_sample_of_queryset(qs, 9, User),
+            instance=self.get_sample_of_queryset(qs, 9, User)
+                .order_by('-fans_amount').order_by('-post_amount'),
             many=True,
             context={'request': request}
         ).data
