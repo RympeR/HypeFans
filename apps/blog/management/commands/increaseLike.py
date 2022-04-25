c7a14598df816ffabd4122488a61b2fa414a5182
George <georg.rashkov@gmail.com>
2022-04-25 16:36:06 +0300

    post action create

--- apps/blog/management/commands/increaseLike.py
+++ apps/blog/management/commands/increaseLike.py
@@ -4,14 +4,18 @@ from django.core.management import BaseCommand
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('like_amount', nargs='+', type=int)
+        parser.add_argument('post_id', nargs='+', type=int)
+    
     def handle(self, *args, **options):
+        
+
         # Create like post action object in range of passed argument for args
-        print(args)
-        self.stdout.write(args)
-        for _ in range(int(args[0])):
+        for _ in range(int(**options['like_amount'])):
             PostAction.objects.create(
                 user=User.objects.get(pk=1),
-                post=Post.objects.get(pk=args[1]),
+                post=Post.objects.get(pk=int(**options['post_id'])),
                 like=True
             )
         self.stdout.write(self.style.SUCCESS('Successfully created'))
