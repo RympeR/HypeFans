406cc3033a253d1e40608fe46e856a2ebd222ac8
George <42513185+RympeR@users.noreply.github.com>
2022-05-07 00:06:50 +0300

    main branch rebuild

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,9 @@
-venv\Scripts\activate
 cd frontend
 yarn build
 rmdir -r ../build
 mv -Force ./build ../build
 cd ..
+venv\Scripts\activate
 python .\manage.py collectstatic --no-input
 git add .
 git commit -am "main branch rebuild"
