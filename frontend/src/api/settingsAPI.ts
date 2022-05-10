15cee607508e5c792df88313a69b19bd58f82763
alexeybelyaevv <alex05lev08@gmail.com>
2022-05-10 18:41:07 +0300

    listsUpdate

--- frontend/src/api/settingsAPI.ts
+++ frontend/src/api/settingsAPI.ts
@@ -5,5 +5,30 @@ export const settingsAPI = {
     return instance.get('/blog/get-users-lists/').then((res) => {
       return res.data;
     });
-  }
+  },
+  getSubsList() {
+    return instance.get('/blog/get-users-lists-subs/').then((res) => {
+      return res.data;
+    });
+  },
+  getFriendsList() {
+    return instance.get('/blog/get-users-lists-friends/').then((res) => {
+      return res.data;
+    });
+  },
+  getFavsList() {
+    return instance.get('/blog/get-users-lists-favourites/').then((res) => {
+      return res.data;
+    });
+  },
+  getDonatorsList() {
+    return instance.get('/blog/get-users-lists-donators/').then((res) => {
+      return res.data;
+    });
+  },
+  getBlockedList() {
+    return instance.get('/blog/get-users-lists-blocked/').then((res) => {
+      return res.data;
+    });
+  },
 };
